54af3edd5ec8c209e84a254445d4ce89952e5544
sshariff01 <shoabe.shariff@gmail.com>
2014-06-21 16:35:05 -0400

    Integrate login with facebook via javascript sdk.

--- joyride/givers/views.py
+++ joyride/givers/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 
+#import urllib2
+#import json
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    if request.method == 'POST':
+        return render(request, 'search.html')
 
 def search(request):
     return render(request, 'search.html')
+
+
+#def facebook_login(request):
+#    url = 'https://www.facebook.com/dialog/oauth?client_id=1486567314893292&redirect_uri=https://www.facebook.com/connect/login_success.html&response_type=token&scope=public_profile,email,user_friends'
+#    serialized_data = urllib2.urlopen(url).read()
+
+#    data = json.loads(serialized_data)
+#    r = request.GET("https://www.facebook.com/dialog/oauth?client_id=1486567314893292&redirect_uri=https://www.facebook.com/connect/login_success.html&response_type=token&scope=public_profile,email,user_friends")
+#    return render(request, 'search.html')
