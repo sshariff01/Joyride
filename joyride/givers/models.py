c36c25bbc0ea8716a145197f9293db7aeb942f9f
sshariff01 <shoabe.shariff@gmail.com>
2014-06-21 17:13:32 -0400

    Change field types in Giver model to TextField.

--- joyride/givers/models.py
+++ joyride/givers/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Giver(models.Model):
-    fb_id = models.IntegerField(default=0)
-    lng_start = models.IntegerField(default=0)
-    lat_start = models.IntegerField(default=0)
-    lng_end = models.IntegerField(default=0)
-    lat_end = models.IntegerField(default=0)
+    fb_id = models.TextField(default=0)
+    lng_start = models.TextField(default=0)
+    lat_start = models.TextField(default=0)
+    lng_end = models.TextField(default=0)
+    lat_end = models.TextField(default=0)
     
