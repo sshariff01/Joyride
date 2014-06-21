6d81cf7aa5422c75c7258759e45d715ea8b2ca70
sshariff01 <shoabe.shariff@gmail.com>
2014-06-21 12:49:01 -0400

    Remove givers/urls, merge all urls to main joyride/urls. Create a new 'search' template for main map page.

--- joyride/joyride/urls.py
+++ joyride/joyride/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
+from givers import views as giversviews
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,6 +11,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'joyride.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('givers.urls')),
+    url(r'^$', giversviews.index, name='index'),
+    url(r'^index/', giversviews.index, name='index'), # include('givers.urls')),
+    url(r'^search/', giversviews.search, name='search'), # include('givers.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
