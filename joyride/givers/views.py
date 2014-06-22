2deb12afced27d1dbaa26d95cfa752ecf46cd733
sshariff01 <shoabe.shariff@gmail.com>
2014-06-21 21:15:21 -0400

    Add helper method to calculate distance between two locations. (Currently unused).

--- joyride/givers/views.py
+++ joyride/givers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from givers.models import Giver
+from math import sin, cos, radians, degrees, acos
 
 #import urllib2
 #import json
@@ -22,6 +23,13 @@ def post_user(request):
         g.save()
     return render(request, 'index.html')
 
+def calc_dist(lat_a, long_a, lat_b, long_b):
+    lat_a = radians(lat_a)
+    lat_b = radians(lat_b)
+    long_diff = radians(long_a - long_b)
+    distance = (sin(lat_a) * sin(lat_b) +
+                cos(lat_a) * cos(lat_b) * cos(long_diff))
+    return degrees(acos(distance)) * 69.09
 
 
 #def facebook_login(request):
